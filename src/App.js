941515eac734d48960660173fbad76cdf3254861
Olivier.Sohy <gorgobox@gmail.com>
2018-10-25 15:03:25 +0200

    add App.js + Travels.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+import Travel from "./Travels";
 
 class App extends Component {
   render() {
@@ -8,18 +10,20 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h1 className="App-title">My Wonderful and not interested Travels</h1>
         </header>
+        <Travel
+          destination="Bulgarie"
+          country="Belgium"
+          distance="2000 Km !"
+          photo="https://back-visiteurope.orchestra-platform.com/admin/TS/fckUserFiles/Content_Image/B2C/destinations/page_destination/croatie/voyage-croatie-autotour.jpg"
+        />
+        <Travel
+          destination="Jamaica"
+          country="Belgium"
+          distance="5 Km seulement"
+          photo="https://voyages-auchan.onelist.fr/Portals/24/01.JPG"
+        />
       </div>
     );
   }
